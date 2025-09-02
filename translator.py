b334ba646c28d678d4cdd5e75a184fc9f8494230
arsheer13 <arshee.rizvi@diu.one>
2025-09-02 10:32:27 +0530

    translator.py

--- translator.py
+++ translator.py
@@ -2,11 +2,9 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 model_path = "facebook/nllb-200-distilled-600M"
 
-# Allow downloading from internet if model not present
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
 
-# Language code mapping
 lang_code_to_id = {
     "eng_Latn": tokenizer.convert_tokens_to_ids("__eng_Latn__"),
     "hin_Deva": tokenizer.convert_tokens_to_ids("__hin_Deva__"),
@@ -14,13 +12,7 @@ lang_code_to_id = {
 }
 
 def translate_text(text, src_lang="hin_Deva", tgt_lang="eng_Latn"):
-    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
-    inputs["forced_bos_token_id"] = lang_code_to_id[tgt_lang]
-
-    outputs = model.generate(
-        **inputs,
-        max_length=512,
-        num_beams=4,
-        early_stopping=True
-    )
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+    tokenizer.src_lang = src_lang
+    encoded = tokenizer(text, return_tensors="pt")
+    generated_tokens = model.generate(**encoded, forced_bos_token_id=lang_code_to_id[tgt_lang])
+    return tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
